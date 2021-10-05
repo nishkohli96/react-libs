7e552c0fe3e6586c0f2b77975721a4401c148637
Nishant Kohli <nishkohli96@gmail.com>
2021-10-06 03:56:54 +0530

    redux-persist mostly done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Suspense, StrictMode } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import LoadingComp from '_Components/LoadingComp';
 import Routing from '_Components/Routing';
-import store from '_Store';
+import { store, persistor } from '_Store';
 import '_Styles/tailwind.css';
 
 function App() {
@@ -14,9 +15,14 @@ function App() {
         <StrictMode>
             <Suspense fallback={<LoadingComp />}>
                 <Provider store={store}>
-                    <QueryClientProvider client={queryClient}>
-                        <Routing />
-                    </QueryClientProvider>
+                    <PersistGate
+                        loading={<LoadingComp />}
+                        persistor={persistor}
+                    >
+                        <QueryClientProvider client={queryClient}>
+                            <Routing />
+                        </QueryClientProvider>
+                    </PersistGate>
                 </Provider>
             </Suspense>
         </StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import counterReducer from './counter.slice';
 import bankInfoReducer from './bankInfo.slice';
 import { apiSlice } from './api.slice';
 
-export default configureStore({
-    reducer: {
-        /*  Keys in reducer object must be the same as "name" in their 
-            respective slice */
-        bank: bankInfoReducer,
-        counter: counterReducer,
-        [apiSlice.reducerPath]: apiSlice.reducer,
-    },
+const rootReducer = combineReducers({
+    /*  Keys in reducer object must be the same as "name" in their 
+        respective slice */
+    bank: bankInfoReducer,
+    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = createStore(persistedReducer);
+export const store = configureStore({
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+export const persistor = persistStore(store);
