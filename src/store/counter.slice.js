732132c7f514724846b9ebb2bb6d857c97aed686
Nishant Kohli <nishkohli96@gmail.com>
2021-10-06 04:02:39 +0530

    mostly done

--- src/store/counter.slice.js
+++ src/store/counter.slice.js
@@ -3,12 +3,11 @@ import { createSlice } from '@reduxjs/toolkit';
 export const counterSlice = createSlice({
     name: 'counter',
     initialState: {
-        value: localStorage.getItem('count') ?? 0,
+        value: 0,
     },
     reducers: {
         increment: (state) => {
             state.value += 1;
-            localStorage.setItem('count', state.value);
         },
         decrement: (state) => {
             state.value -= 1;
