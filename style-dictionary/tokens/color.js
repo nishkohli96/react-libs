16b58edf799eef4abb34aebd6ce0e2360e74864b
Nishant Kohli <nishkohli96@gmail.com>
2022-07-16 08:13:38 +0530

    add assets in style-dictionary folder

---
+++ style-dictionary/tokens/color.js
@@ -0,0 +1,19 @@
+module.exports = {
+    color: {
+        base: {
+            primary: {
+                value: '#61dafb',
+            },
+            secondary: {
+                value: '#fc7aa5',
+            },
+        },
+        text: {
+            value: '#666666',
+        },
+        background: {
+            value: '#EEEEEE',
+        },
+        default: { value: 'color.base.primary.value' },
+    },
+};
