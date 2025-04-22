45ff0e72acd7724cf38fe947baff8a76551e5b9e
Nishant <nishkohli96@gmail.com>
2025-04-22 20:19:24 +0530

    upgrade eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   extends: [
     'react-app/jest',
     '@nish1896/eslint-config/js',
-    '@nish1896/eslint-config/react'   
+    '@nish1896/eslint-config/jsx'   
   ],
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    'react/prop-types': 'off'
-  }
+  // rules: {
+  //   'react/jsx-filename-extension': 'off',
+  //   'react/prop-types': 'off'
+  // }
 };
